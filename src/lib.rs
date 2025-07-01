c4e131c8fb62ebc4edc442e4f7b465128204a8f9
Jakub Jelen <jjelen@redhat.com>
2025-07-01 10:24:31 +0200

    Add API to get return a signature's salt
      - Version 6 signatures include a salt that must be hashed before any
        data is hashed.
    
      - Add a function, `pgpDigParamsSalt`, to return a signature's salt.
    
      - See #91.
    
      - See https://github.com/rpm-software-management/rpm/issues/3846
    
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -591,6 +591,43 @@ fn _pgpDigParamsCreationTime(dig: *const PgpDigParams) -> u32[0] {
 });
 
 ffi!(
+/// Returns a signature's salt.
+///
+/// Version 6 signatures are salted.  The salt needs to be fed into
+/// the digest buffer before the actual message, which is processed
+/// already by RPM.
+///
+/// The caller must *not* free the returned buffer.
+///
+/// Returns an error if the signature does not include a salt.
+/// Version 6 signatures always have a salt; version 3 and version 4
+/// signatures never have a salt and thus will always return an error.
+fn _pgpDigParamsSalt(dig: *const PgpDigParams,
+                     datap: *mut *const u8,
+                     lenp: *mut size_t)
+    -> ErrorCode
+{
+    let dig = check_ptr!(dig);
+    let datap = check_mut!(datap);
+    let lenp = check_mut!(lenp);
+
+    let sig = dig.signature().ok_or_else(|| {
+        Error::Fail("sig parameter does not designate a signature".into())
+    })?;
+
+    if let Some(salt) = sig.salt() {
+        t!("Salt: {}",
+           salt.iter().map(|v| format!("{:02X}", v)).collect::<String>());
+        *lenp = salt.len() as size_t;
+        *datap = salt.as_ptr();
+        Ok(())
+    } else {
+        Err(Error::Fail("The provided signature does not have any salt".into()))
+    }
+});
+
+
+ffi!(
 /// Verifies the signature.
 ///
 /// If `key` is `NULL`, then this computes the hash and checks it
